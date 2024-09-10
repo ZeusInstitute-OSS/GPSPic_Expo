ca542d45c210d1e76f5f853c3aa3fedba0ccac70
SoundDrill 31 <sounddrill31@gmail.com>
2024-09-10 16:55:16 +0000

    Initial web support

--- hooks/usePermissions.ts
+++ hooks/usePermissions.ts
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Camera } from 'expo-camera/legacy';
 import * as Location from 'expo-location';
 import * as MediaLibrary from 'expo-media-library';
+import { Platform } from 'react-native';
 
 export default function usePermissions() {
   const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
@@ -9,7 +10,12 @@ export default function usePermissions() {
   const requestPermissions = async () => {
     const cameraStatus = await Camera.requestCameraPermissionsAsync();
     const locationStatus = await Location.requestForegroundPermissionsAsync();
-    const mediaLibraryStatus = await MediaLibrary.requestPermissionsAsync();
+    let mediaLibraryStatus;
+    if (Platform.OS !== 'web') {
+      mediaLibraryStatus = await MediaLibrary.requestPermissionsAsync();
+    } else {
+      mediaLibraryStatus = { status: 'granted' }; // assume granted on web
+    }
 
     setHasPermissions(
       cameraStatus.status === 'granted' &&
